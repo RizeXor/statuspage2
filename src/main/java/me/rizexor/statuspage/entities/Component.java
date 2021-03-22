f18f9450283ff68973ecb00f60813cf84ac304ca
RizeXor <decodestudios.help@gmail.com>
2021-03-22 20:47:57 +0530

    potty

--- src/main/java/me/rizexor/statuspage/entities/Component.java
+++ src/main/java/me/rizexor/statuspage/entities/Component.java
@@ -24,7 +24,7 @@ public class Component implements Serializable {
     private Long id;
     @Column(nullable = false)
     private String name;
-    @Column()
+    @Column(nullable = false)
     private ComponentStatus status;
 
 }
